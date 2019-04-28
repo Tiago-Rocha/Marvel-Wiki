2c02800e731a4bf780d2c02ff178a9b47e346376
Tiago Rocha <tiago.aziz@gmail.com>
2019-04-28 23:42:56 +0100

    Updates API client dateformat to be used on encoding and decoding

--- Marvel-Wiki/Storage/API/SwaggerGenerated/CodableHelper.swift
+++ Marvel-Wiki/Storage/API/SwaggerGenerated/CodableHelper.swift
@@ -26,7 +26,7 @@ open class CodableHelper {
             formatter.calendar = Calendar(identifier: .iso8601)
             formatter.locale = Locale(identifier: "en_US_POSIX")
             formatter.timeZone = TimeZone(secondsFromGMT: 0)
-            formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSXXXXX"
+            formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss-SSSS"
             decoder.dateDecodingStrategy = .formatted(formatter)
         }
 
