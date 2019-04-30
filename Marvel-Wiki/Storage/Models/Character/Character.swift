8bd195c0346fbc7298c3a6afafdf93e82cba1ebe
Tiago Rocha <tiago.aziz@gmail.com>
2019-04-30 23:23:57 +0100

    Implements Domain Character and its builder

--- Marvel-Wiki/Storage/Models/Character/Character.swift
+++ Marvel-Wiki/Storage/Models/Character/Character.swift
@@ -1,2 +1,27 @@
 import Foundation
 
+class Character {
+    var id: Int
+    
+    var name: String
+    
+    var description: String?
+    
+    var imageURL: String?
+    
+    var imageFormat: String?
+    
+    init(id: Int, name: String, _ description: String?, _ imageURL: String?, _ imageFormat: String?) {
+        
+        self.id = id
+        self.name = name
+        self.description = description
+        self.imageURL = imageURL
+        self.imageFormat = imageFormat
+    }
+    
+    public static func builder() -> CharacterBuilder {
+        
+        return CharacterBuilder()
+    }
+}
--- Marvel-Wiki/Storage/Models/Character/CharacterBuilder.swift
+++ Marvel-Wiki/Storage/Models/Character/CharacterBuilder.swift
@@ -1,7 +1,73 @@
-
-
 import Foundation
 
 class CharacterBuilder {
+
+    var id: Int?
     
+    var name: String?
+    
+    var description: String?
+    
+    var imageURL: String?
+    
+    var imageExt: String?
+    
+    func with(id: Int?) -> Self {
+        
+        self.id = id
+        return self
+    }
+    
+    func with(name: String?) -> Self {
+        
+        self.name = name
+        return self
+    }
+    
+    func with(description: String?) -> Self {
+        
+        self.description = description
+        return self
+    }
+    
+    func with(imageURL: String?) -> Self {
+        
+        self.imageURL = imageURL
+        return self
+    }
+    
+    func with(imageExt: String?) -> Self {
+        
+        self.imageExt = imageExt
+        return self
+    }
+    
+    func with(character: ModelCharacter) -> Self {
+        
+        _ = with(id: character._id)
+            .with(name: character.name)
+            .with(imageURL: character.thumbnail?.path)
+            .with(description: character._description)
+            .with(imageExt: character.thumbnail?._extension)
+        
+        return self
+    }
+    
+    func build() throws  -> Character {
+        
+        guard let _id = id else {
+            
+            throw BuilderError.fieldNotSet("Character.id")
+        }
+        
+        guard let _name = name else {
+            throw BuilderError.fieldNotSet("Character.name")
+        }
+        
+        return Character(id: _id, name: _name, description, imageURL, imageExt)
+    }
+}
+
+public enum BuilderError: Error {
+    case fieldNotSet(String)
 }
