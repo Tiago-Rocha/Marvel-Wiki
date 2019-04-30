8d1ae259d83d4a066150a1efb62e851277ff8b48
Tiago Rocha <tiago.aziz@gmail.com>
2019-04-30 23:24:33 +0100

    Adds foo code to test API request and image assignment

--- Marvel-Wiki/ViewController.swift
+++ Marvel-Wiki/ViewController.swift
@@ -7,29 +7,43 @@
 //
 
 import UIKit
+import Kingfisher
 
 class ViewController: UIViewController {
-
+    
     var mAPi: MarvelAPI?
+    
+    @IBOutlet weak var imageView: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         let clientConfig = SwaggerClientAPI(basePath: "https://gateway.marvel.com", credential: nil)
         mAPi = MarvelAPI(config: clientConfig,
-                  factory: AlamofireRequestBuilderFactory(),
-                  authentication: MarvelAuthentication())
-        
-
+                         factory: AlamofireRequestBuilderFactory(),
+                         authentication: MarvelAuthentication())
     }
+    
     @IBAction func cl(_ sender: Any) {
+        
         mAPi!.getCreatorCollection(name: nil, nameStartsWith: nil, modifiedSince: nil, comics: nil, series: nil, events: nil, stories: nil, orderBy: nil, limit: 10, offset: 10) { wrapper, error in
             
-            wrapper
-            print(wrapper)
-            print(error)
-            print("Returned call")
+            guard
+                let _characterModel = wrapper?.data?.results?[0],
+                let _character = try? Character.builder().with(character: _characterModel).build(),
+                let _imageURL = _character.imageURL,
+                let _imageFormat = _character.imageFormat
+                else {
+                    print("failed to build character")
+                    return
+            }
             
+            guard let _url = URL(string: _imageURL + "/portrait_xlarge." + _imageFormat) else {
+                print("yo no url")
+                return
+            }
+
+            self.imageView.kf.setImage(with: ImageResource(downloadURL: _url))
         }
     }
 }
