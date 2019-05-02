877fb44e13e18b3b7150ce148def6e506834def5
Tiago <tiago.rocha@vision-box.com>
2019-05-02 15:15:35 +0100

    Adds DependencyGraph and CharacterRepositoryObserver base file

---
+++ Marvel-Wiki/Storage/Repositories/CharacterRepository/CharacterRepositoryObserver.swift
@@ -0,0 +1,9 @@
+//
+//  CharacterRepositoryObserver.swift
+//  Marvel-Wiki
+//
+//  Created by Tiago Rocha on 02/05/2019.
+//  Copyright © 2019 Tiago Rocha. All rights reserved.
+//
+
+import Foundation
