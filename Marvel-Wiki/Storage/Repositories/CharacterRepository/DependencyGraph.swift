a8738dbb62d78f1b59179c00d23ac511a4d7768f
Tiago <tiago.rocha@vision-box.com>
2019-05-02 15:16:25 +0100

    Implements DependencyGraph

--- Marvel-Wiki/Storage/Repositories/CharacterRepository/DependencyGraph.swift
+++ Marvel-Wiki/Storage/Repositories/CharacterRepository/DependencyGraph.swift
@@ -1,9 +1,38 @@
-//
-//  DependencyGraph.swift
-//  Marvel-Wiki
-//
-//  Created by Tiago Rocha on 02/05/2019.
-//  Copyright © 2019 Tiago Rocha. All rights reserved.
-//
-
 import Foundation
+
+class DependencyGraph {
+    
+    var characterRepository: CharacterRepository?
+    
+    static let shared = DependencyGraph()
+    
+    func getCharacterListViewController() -> CharacterListViewController {
+        
+        return CharacterListViewController(viewModel: getCharacterListViewModel())
+    }
+    
+    func getCharacterListViewModel() -> CharacterListViewModel {
+        return CharacterListViewModel(characterRepository: getCharacterRepository())
+    }
+    
+    func getCharacterRepository() -> CharacterRepository {
+        
+        guard let _ = characterRepository else {
+            characterRepository = CharacterRepository(marvelAPI: getMarvelAPI())
+            return characterRepository!
+        }
+        return CharacterRepository(marvelAPI: getMarvelAPI())
+    }
+    
+    func getMarvelAPI() -> MarvelAPI {
+        
+        return MarvelAPI(config: getMarvelAPIClientConfig()
+            , factory: AlamofireRequestBuilderFactory()
+            , authentication: MarvelAuthentication())
+    }
+    
+    func getMarvelAPIClientConfig() -> SwaggerClientAPI {
+        
+        return SwaggerClientAPI(basePath: "https://gateway.marvel.com", credential: nil)
+    }
+}
